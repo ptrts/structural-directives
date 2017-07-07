11e6aef38a7b88c9d5a8b7da7a790f4163d0faf8
Pavel Taruts <ptrts@mail.ru>
2017-07-07 22:22:42 +0700

    Комментарии

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -21,18 +21,39 @@ import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
 @Directive({selector: '[myUnless]'})
 export class UnlessDirective {
 
+    // Наша собственная переменная, которая показывает, выведен ли сейчас шаблон в представление,
+    // или в представлении у нас сейчас ничего нет
     private hasView = false;
 
     constructor(private templateRef: TemplateRef<any>,
                 private viewContainer: ViewContainerRef) {
     }
 
-    @Input() set myUnless(condition: boolean) {
+    // Наверное, то что здесь стоит директива @Input - это означает, что каждый раз, когда соответствующее значение
+    // будет обновляться, то каждый раз будет вызываться этот метод.
+    // Метод будет вызываться даже тогда, когда новое значение выражения - такое же как и предыдущее.
+    @Input()
+    set myUnless(condition: boolean) {
+
         if (!condition && !this.hasView) {
+
+            // false и !this.hasView
+
+            // В переданном нам контейнере представления, создаем представление,
+            // используя для этого просто наш весь шаблон
             this.viewContainer.createEmbeddedView(this.templateRef);
+
+            // Записываем себе флажок, что да, в контейнер представления выведен наш шаблон
             this.hasView = true;
+
         } else if (condition && this.hasView) {
+
+            // true && this.hasView
+
+            // Удаляем из контейнера представления все что там есть
             this.viewContainer.clear();
+
+            // Пишем себе флажок, что в контейнере представления у нас ничего нету
             this.hasView = false;
         }
     }
